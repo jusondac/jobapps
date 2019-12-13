62a099f6543c0f78eda5fffb094199e1bedad999
jusondac <rejkamuhammad@gmail.com>
2019-12-13 20:57:05 +0700

    beautifull view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
+  get 'users/edit'
   root 'joblists#index'
   resources :joblists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
