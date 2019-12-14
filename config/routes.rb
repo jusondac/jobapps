a57c6719779f8d5b2795308d43e52d7989b03a25
jusondac <rejkamuhammad@gmail.com>
2019-12-14 18:39:53 +0700

    register without bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/edit'
+  get 'sign_up' =>'users#new',as:'sign_up'
   root 'joblists#index'
+  resources :users
   resources :joblists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
